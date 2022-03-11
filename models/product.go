11f004046b186645909e41814a8eaf3e11694e0c
Sirin k <sirinibin2006@gmail.com>
2022-03-11 17:31:46 +0300

    Enhancements

--- models/product.go
+++ models/product.go
@@ -534,7 +534,7 @@ func SearchProduct(w http.ResponseWriter, r *http.Request) (products []Product, 
 			return products, criterias, errors.New("Cursor decode error:" + err.Error())
 		}
 
-		product.SearchLabel = product.Name + "  Part#" + product.PartNumber + ", Barcode:" + product.BarCode + ", Arabic:" + product.NameInArabic + ")"
+		product.SearchLabel = product.Name + "(Part#" + product.PartNumber + ", Barcode:" + product.BarCode + ", Arabic:" + product.NameInArabic + ")"
 
 		if _, ok := criterias.Select["category.id"]; ok {
 			for _, categoryID := range product.CategoryID {
