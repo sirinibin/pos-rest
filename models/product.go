921a178d2fa797b24b0f54d24ba1061deb5a3e61
Sirin k <sirinibin2006@gmail.com>
2022-03-23 18:46:50 +0300

    Enhancements

--- models/product.go
+++ models/product.go
@@ -774,7 +774,7 @@ func (product *Product) GenerateBarCode(startFrom int) (string, error) {
 
 func (product *Product) Insert() (err error) {
 	collection := db.Client().Database(db.GetPosDB()).Collection("product")
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	product.ID = primitive.NewObjectID()
 	if len(product.ItemCode) == 0 {
