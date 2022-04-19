9250d16d61aaa27cf5ce2aeb433b85b0ee6f5b09
Sirin k <sirinibin2006@gmail.com>
2022-04-19 14:38:48 +0300

    Enhancements

--- models/common.go
+++ models/common.go
@@ -23,7 +23,17 @@ func GetTotalCount(filter map[string]interface{}, collectionName string) (count 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	return collection.CountDocuments(ctx, filter)
+	log.Print("filter:")
+	log.Print(filter)
+
+	filter["deleted"] = bson.M{"$ne": true}
+
+	if len(filter) == 0 {
+		return collection.CountDocuments(ctx, filter)
+	} else {
+		return collection.CountDocuments(ctx, filter)
+	}
+
 }
 
 func TrimLogicalOperatorPrefix(str string) string {
