6a3ec2c0050a29b03f5b2b801c95e6008f01aeea
sirin kolasseri <sirin@sirins-MacBook-Air.local>
2023-04-14 09:16:48 +0530

    Fix

--- models/product.go
+++ models/product.go
@@ -1312,7 +1312,7 @@ func ProcessProducts() error {
 						for _, unitPrice := range model.UnitPrices {
 							if unitPrice.StoreID == *order.StoreID &&
 								order.Products[k].ProductID.Hex() == model.ID.Hex() &&
-								order.Products[k].Loss > 0 {
+								(order.Products[k].Loss > 0 || order.Products[k].Profit <= 0) {
 								//log.Printf("Updating purchase unit price of order: %s", order.Code)
 								order.Products[k].PurchaseUnitPrice = unitPrice.PurchaseUnitPrice
 							}
