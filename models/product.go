350d1dbefc8baf6f4d84e0564941b4180992178b
sirin kolasseri <sirin@sirins-MacBook-Air.local>
2023-12-18 04:05:33 +0530

    Enhancements

--- models/product.go
+++ models/product.go
@@ -93,7 +93,7 @@ type Product struct {
 	Category     []*ProductCategory    `json:"category,omitempty"`
 	//UnitPrices    []ProductUnitPrice    `bson:"unit_prices,omitempty" json:"unit_prices,omitempty"`
 	//Stock         []ProductStock        `bson:"stock,omitempty" json:"stock,omitempty"`
-	//Stores        []ProductStore          `bson:"stores,omitempty" json:"stores,omitempty"`
+	Stores        []ProductStore          `bson:"stores,omitempty" json:"stores,omitempty"`
 	ProductStores map[string]ProductStore `bson:"product_stores,omitempty" json:"product_stores,omitempty"`
 	Unit          string                  `bson:"unit" json:"unit"`
 	Images        []string                `bson:"images,omitempty" json:"images,omitempty"`
@@ -2325,7 +2325,7 @@ func ProcessProducts() error {
 			product.ProductStores = map[string]ProductStore{}
 		}
 
-		for _, store := range product.ProductStores {
+		for _, store := range product.Stores {
 			if !store.StoreID.IsZero() && store.StoreID.Hex() != "" {
 				product.ProductStores[store.StoreID.Hex()] = store
 			}
