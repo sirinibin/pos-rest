4c83f358865967a7d4dee991607c51eed5781761
sirin kolasseri <sirin@sirins-MacBook-Air.local>
2024-03-27 21:13:59 +0530

    Enhancements

--- main.go
+++ main.go
@@ -14,7 +14,6 @@ import (
 	"github.com/sirinibin/pos-rest/controller"
 	"github.com/sirinibin/pos-rest/db"
 	"github.com/sirinibin/pos-rest/env"
-	"github.com/sirinibin/pos-rest/models"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"gopkg.in/mgo.v2/bson"
@@ -703,11 +702,12 @@ func cronJobsEveryHour() error {
 			log.Print(err)
 		}
 	*/
-
-	err := models.ProcessOrders()
-	if err != nil {
-		log.Print(err)
-	}
+	/*
+		err := models.ProcessOrders()
+		if err != nil {
+			log.Print(err)
+		}
+	*/
 
 	/*
 		err := models.ProcessPostings()
