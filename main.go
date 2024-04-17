cdb1d4ccba2ead9b12aa65864c1f53aa1fa8a3fb
sirin kolasseri <sirin@sirins-MacBook-Air.local>
2024-04-17 16:16:12 +0530

    Enhancements

--- main.go
+++ main.go
@@ -14,7 +14,6 @@ import (
 	"github.com/sirinibin/pos-rest/controller"
 	"github.com/sirinibin/pos-rest/db"
 	"github.com/sirinibin/pos-rest/env"
-	"github.com/sirinibin/pos-rest/models"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"gopkg.in/mgo.v2/bson"
@@ -593,39 +592,39 @@ func cronJobsEveryHour() error {
 	*/
 
 	/*
-		err := models.ProcessCapitals()
+			err := models.ProcessCapitals()
+			if err != nil {
+				log.Print(err)
+			}
+
+			err = models.ProcessPurchases()
+			if err != nil {
+				log.Print(err)
+			}
+
+			err = models.ProcessPurchaseReturns()
+			if err != nil {
+				log.Print(err)
+			}
+
+			err = models.ProcessOrders()
+			if err != nil {
+				log.Print(err)
+			}
+
+
+
+
+		err := models.ProcessSalesReturns()
 		if err != nil {
 			log.Print(err)
 		}
 
-		err = models.ProcessPurchases()
-		if err != nil {
-			log.Print(err)
-		}
-
-		err = models.ProcessPurchaseReturns()
-		if err != nil {
-			log.Print(err)
-		}
-
-		err = models.ProcessOrders()
-		if err != nil {
-			log.Print(err)
-		}
+		/*
 
 
 
 	*/
-	err := models.ProcessSalesReturns()
-	if err != nil {
-		log.Print(err)
-	}
-
-	/*
-
-
-
-	 */
 
 	/*
 
