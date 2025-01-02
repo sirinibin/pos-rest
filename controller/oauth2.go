118744e826686cadc45ca78d23479ff361fa47c0
sirin kolasseri <sirinibin2006@gmail.com>
2025-01-02 07:35:07 +0530

    checking-4

--- controller/oauth2.go
+++ controller/oauth2.go
@@ -117,7 +117,7 @@ func APIInfo(w http.ResponseWriter, r *http.Request) {
 
 	var response models.Response
 	response.Status = true
-	response.Result = "GoLang / MongoDb Microservice-jj [ OAuth2, JWT and Redis used for security ] "
+	response.Result = "GoLang / MongoDb Microservice-jj-kk [ OAuth2, JWT and Redis used for security ] "
 
 	json.NewEncoder(w).Encode(response)
 }
