c4ce06bb8f1cc101618f636bf8f3cbf80f3cf2fa
sirin kolasseri <sirinibin2006@gmail.com>
2025-07-19 16:59:18 +0300

    Fix

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 	"github.com/sirinibin/pos-rest/controller"
 	"github.com/sirinibin/pos-rest/db"
 	"github.com/sirinibin/pos-rest/env"
+	"github.com/sirinibin/pos-rest/models"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -431,41 +432,41 @@ func ListAllIndexes(collectionName string) {
 
 func cronJobsEveryHour() error {
 	log.Print("Cron job is set to run every 8 hours")
-	/*
-		err := models.ProcessOrders()
-		if err != nil {
-			log.Print(err)
-		}
 
-		err = models.ProcessSalesReturns()
-		if err != nil {
-			log.Print(err)
-		}
+	err := models.ProcessOrders()
+	if err != nil {
+		log.Print(err)
+	}
 
-		err = models.ProcessPurchases()
-		if err != nil {
-			log.Print(err)
-		}
+	err = models.ProcessSalesReturns()
+	if err != nil {
+		log.Print(err)
+	}
 
-		err = models.ProcessPurchaseReturns()
-		if err != nil {
-			log.Print(err)
-		}
+	err = models.ProcessPurchases()
+	if err != nil {
+		log.Print(err)
+	}
 
-		err = models.ProcessQuotations()
-		if err != nil {
-			log.Print(err)
-		}
+	err = models.ProcessPurchaseReturns()
+	if err != nil {
+		log.Print(err)
+	}
 
-		err = models.ProcessQuotationSalesReturns()
-		if err != nil {
-			log.Print(err)
-		}
+	err = models.ProcessQuotations()
+	if err != nil {
+		log.Print(err)
+	}
 
-		err = models.ProcessDeliveryNotes()
-		if err != nil {
-			log.Print(err)
-		}*/
+	err = models.ProcessQuotationSalesReturns()
+	if err != nil {
+		log.Print(err)
+	}
+
+	err = models.ProcessDeliveryNotes()
+	if err != nil {
+		log.Print(err)
+	}
 
 	/*
 		err = models.ProcessCustomerWithdrawals()
