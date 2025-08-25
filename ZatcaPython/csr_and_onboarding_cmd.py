01df58f98226cdf7395e3cff7b02e2f9dbc0eeb4
sirin kolasseri <sirinibin2006@gmail.com>
2025-08-25 14:58:48 +0300

    Zatca xml onboarding fix

--- ZatcaPython/csr_and_onboarding_cmd.py
+++ ZatcaPython/csr_and_onboarding_cmd.py
@@ -15,7 +15,7 @@ def main():
     environment_type = 'Simulation'
 
     OTP = '682222'  # For Simulation and Production Get OTP from fatooraPortal
-    store_type = 'store2'  #test | store1 | store2
+    store_type = 'test'  #test | store1 | store2
 
     csr_config = {}
     if store_type == 'test':
