2cd8911f6e4c8077c68ea6ecd838f47af17690ed
sirin kolasseri <sirinibin2006@gmail.com>
2025-11-05 18:45:20 +0300

    FIxing: product purchase history

--- models/product_history.go
+++ models/product_history.go
@@ -1160,7 +1160,7 @@ func (salesReturn *SalesReturn) CreateProductsHistory() error {
 	}
 
 	collection := db.GetDB("store_" + salesReturn.StoreID.Hex()).Collection("product_history")
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
 	defer cancel()
 
 	for _, salesReturnProduct := range salesReturn.Products {
